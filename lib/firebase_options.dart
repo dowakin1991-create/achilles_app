4aa1c5372ad3e977566021b6504ed25a68a0f440
dowakin1991-create <dowakin1991@gmail.com>
2026-02-25 22:59:16 +0200

    Create firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: 'AIzaSyDjT5UjvgTs396KGsVtPrOEUrnkUi6lD1I',
+      appId: '1:842206429977:web:eb78b2a92587395999bb88',
+      messagingSenderId: '842206429977',
+      projectId: 'achilles-8de53',
+      authDomain: 'achilles-8de53.firebaseapp.com',
+      storageBucket: 'achilles-8de53.firebasestorage.app',
+    );
+  }
+}
